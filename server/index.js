5c0afa8c91b5a59c06c85637f481fa22117b55ef
abhilasha <abhilashaduwal@gmail.com>
2025-12-23 19:05:52 +0545

    change the path for build

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Catch-all for React Router
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Use EB port
