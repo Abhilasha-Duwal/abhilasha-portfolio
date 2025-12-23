30aa4b866a79d50ff59bafb9a9e5910ee462d53f
abhilasha <abhilashaduwal@gmail.com>
2025-12-23 18:40:59 +0545

    change the route

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // API routes
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.send("Welcome to My MERN Portfolio API!");
 });
 app.use("/api/v1/portfolio", portfolioRouter);
